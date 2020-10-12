167f35a476f50ea1732a716b95423d70c52d38dd
desire <desire@localhost.localdomain>
2020-10-12 16:24:10 +0200

    deploy ionic app version that works!

---
+++ README.md
@@ -0,0 +1,21 @@
+# Ionic Angular
+## test
+
+[source](http://desireyavro.tech/projects.html)
+[inspiration](https://www.linkedin.com/learning/ionic-4-0-essential-training/) 
+
+
+## Scripts
+
+## sudo npm install ionic -g
+Then check the version installed by running `ionic -v` (5.4.16)
+
+### ionic start
+
+### ionic serve
+
+Note. you might have to reinstall node with the latest version
+### sudo npm install n -g
+or with Node Version Manager - NVM,
+### nvm install v14.13.1 
+(latest)
---
+++ app/src/app/activity-detail/activity-detail.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Activity } from '../types';
+import { ActivityService } from '../activity.service';
+import { ActivatedRoute } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { ActivityVideoPage } from '../activity-video/activity-video.page';
+
+import { SocialSharing } from "@ionic-native/social-sharing/ngx";
+
+@Component({
+  selector: 'app-activity-detail',
+  templateUrl: './activity-detail.page.html',
+  styleUrls: ['./activity-detail.page.scss'],
+})
+export class ActivityDetailPage implements OnInit {
+  activityDetail: Observable<Activity>;
+
+  constructor(
+    private _socialShare: SocialSharing,
+    private _modalController: ModalController,
+    activityService: ActivityService,
+    activatedRoute: ActivatedRoute
+    ) { 
+      const activityID = activatedRoute.snapshot.params["activityID"];
+      this.activityDetail = activityService.getActivity(activityID);
+    }
+
+  ngOnInit() {
+  }
+
+  share(){
+    this.activityDetail.subscribe((activity)=> {
+      this._socialShare.share("Look What I Found On This App Called Rana", activity.name, "", activity.cropped);
+    });
+  }
+
+  async openModal(){
+    const videoModal = await this._modalController.create({
+      component: ActivityVideoPage
+    });
+
+    return await this.activityDetail.subscribe((activity)=> {
+      videoModal.componentProps = {
+        videoURL: activity.video_url
+      };
+
+      return videoModal.present();
+    });
+
+  }
+
+}
